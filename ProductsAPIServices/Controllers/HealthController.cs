05b211ed20284fe5ef4ebf4b6996371a508e8a9d
kasem <kasem.vaew@gmail.com>
2019-03-06 21:17:35 +0700

    Add API health check

---
+++ ProductsAPIServices/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductsAPIServices.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet("")]
+        [HttpHead("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
