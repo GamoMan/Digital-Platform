be3dc2ea497e14af90ca725f68297ab22b8009f5
kasem <kasem.vaew@gmail.com>
2019-03-06 21:14:18 +0700

    Upgrade to dotnet core 2.2

--- APIGateway/Startup.cs
+++ APIGateway/Startup.cs
@@ -1,14 +1,19 @@
-﻿namespace APIGateway
-{    
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APIGateway
+{
     using Ocelot.DependencyInjection;
     using Ocelot.Middleware;
-    using System;
-    using System.Text;
-    using Microsoft.IdentityModel.Tokens;
 
     public class Startup
     {
